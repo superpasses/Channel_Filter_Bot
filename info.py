b7b8de2baaeb62f44f7769c233414a540e4a3397
superpasses <165167006+superpasses@users.noreply.github.com>
2024-07-04 06:25:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,7 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+API_ID       = 20562331
+API_HASH     = "9e3e4148e73756a85b95fc69980b678d"
+BOT_TOKEN    = "7264254030:AAGthiCzHc38jg1JdrTej7hy7yg3NNsUl9Y"
+DATABASE_URI = "mongodb+srv://prasenchutia6:4Lyjs25XZIrztCLX@cluster0.jzgd8su.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+LOG_CHANNEL  = -1002200066532
+ADMIN        = 2107586423
+CHANNEL      = "-1002155169038"
